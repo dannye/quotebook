775f61ced9456b84c9db7f5cfab6b2fea35d170c
dannye <corrnondacqb@yahoo.com>
2016-11-02 16:39:28 -0500

    Remove local movie images and increase the image varchar lenght from 50 to 200

--- db/quotebook.sql
+++ db/quotebook.sql
@@ -28,7 +28,7 @@ CREATE TABLE `quotes` (
   `character` varchar(50) DEFAULT NULL,
   `actor` varchar(50) DEFAULT NULL,
   `title` varchar(50) DEFAULT NULL,
-  `image` varchar(50) DEFAULT NULL,
+  `image` varchar(200) DEFAULT NULL,
   `medium` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
@@ -40,7 +40,7 @@ CREATE TABLE `quotes` (
 
 LOCK TABLES `quotes` WRITE;
 /*!40000 ALTER TABLE `quotes` DISABLE KEYS */;
-INSERT INTO `quotes` VALUES (1,'Do you ever dream, Forrest, about who you\'re gonna be?','Jenny Curran','Robin Wright','Forrest Gump','../images/forrest_gump.png','movie'),(2,'You drop a pass, you run a mile. You miss a blocking assignment, you run a mile. You fumble the football and I will break my foot off in your John Brown hind parts...and then you will run a mile.','Herman Boone','Denzel Washington','Remember the Titans','../images/remember_the_titans.png','movie'),(3,'Run, Forrest! Run!','Jenny Curran','Robin Wright','Forrest Gump','../images/forrest_gump.png','movie'),(4,'Look at us. I\'m frozen and you\'re dead, and I love you.','David Aames','Tom Cruise','Vanilla Sky','http://www.gstatic.com/tv/thumb/movieposters/28886','movie');
+INSERT INTO `quotes` VALUES (1,'Do you ever dream, Forrest, about who you\'re gonna be?','Jenny Curran','Robin Wright','Forrest Gump','http://www.wichitaorpheum.com/wp-content/uploads/2013/12/Forrest-Gump-Poster.jpg','movie'),(2,'You drop a pass, you run a mile. You miss a blocking assignment, you run a mile. You fumble the football and I will break my foot off in your John Brown hind parts...and then you will run a mile.','Herman Boone','Denzel Washington','Remember the Titans','http://www.gstatic.com/tv/thumb/dvdboxart/25848/p25848_d_v8_aa.jpg','movie'),(3,'Run, Forrest! Run!','Jenny Curran','Robin Wright','Forrest Gump','http://www.wichitaorpheum.com/wp-content/uploads/2013/12/Forrest-Gump-Poster.jpg','movie'),(4,'Look at us. I\'m frozen and you\'re dead, and I love you.','David Aames','Tom Cruise','Vanilla Sky','http://www.gstatic.com/tv/thumb/movieposters/28886/p28886_p_v8_aa.jpg','movie');
 /*!40000 ALTER TABLE `quotes` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -53,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-11-02 16:16:22
+-- Dump completed on 2016-11-02 16:37:43
